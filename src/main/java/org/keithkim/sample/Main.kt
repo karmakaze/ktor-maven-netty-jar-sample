1deaae3758dbee94d270a55ebca49bb25f4a8b98
helpful <karmakaze@users.noreply.github.com>
2019-01-14 13:11:36 -0500

    Can now 'mvn exec:java' and responds with JSON
    Signed-off-by: Keith Kim <keith@helpful.com>

--- src/main/java/org/keithkim/sample/Main.kt
+++ src/main/java/org/keithkim/sample/Main.kt
@@ -1,19 +1,42 @@
 package org.keithkim.sample
 
 import io.ktor.application.*
-import io.ktor.http.*
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.DefaultHeaders
+import io.ktor.gson.gson
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import java.text.DateFormat
+import java.time.Instant
 
 fun main(args: Array<String>) {
-    val server = embeddedServer(Netty, 8080) {
-        routing {
-            get("/") {
-                call.respondText("Hello, world!", ContentType.Text.Html)
-            }
+    val server = embeddedServer(
+            Netty,
+            watchPaths = listOf("target/classes"),
+            port = 8080,
+            module = Application::mymodule
+    )
+    server.start(wait = true)
+}
+
+fun Application.mymodule() {
+    install(DefaultHeaders)
+    install(ContentNegotiation) {
+        gson {
+            setDateFormat(DateFormat.LONG)
+            setPrettyPrinting()
         }
     }
-    server.start(wait = true)
+
+    routing {
+        get("/") {
+            log.info("Entered GET /")
+            call.respond(hashMapOf(
+                    "message" to "Hello, new world.",
+                    "now" to Instant.now().toString()))
+            log.info("exiting GET /")
+        }
+    }
 }
